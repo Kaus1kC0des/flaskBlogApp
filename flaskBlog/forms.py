4897d7c70aabf30e042a0dc4cbc93e0ef308faf0
Kausik D <kausikdevanathan@gmail.com>
2024-12-02 14:47:35 +0530

    Account Updation feature and Profile pic updation

--- flaskBlog/forms.py
+++ flaskBlog/forms.py
@@ -1,8 +1,9 @@
-from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, EmailField, PasswordField, BooleanField
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import Length, DataRequired, Email, EqualTo, ValidationError
-from email_validator import validate_email
 from flaskBlog.models import User
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -28,14 +29,12 @@ class RegistrationForm(FlaskForm):
 
     submit = SubmitField('Sign Up')
 
-    @staticmethod
-    def validate_username(username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken, please choose another username.')
 
-    @staticmethod
-    def validate_email(self, email):
+    def validate_email(self,email):
         email = User.query.filter_by(email=email.data).first()
         if email:
             raise ValidationError("An User with that email exists. Please provide a different email!")
@@ -55,3 +54,30 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
 
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=2, max=29)]
+    )
+    email = EmailField(
+        'Email',
+        validators=[DataRequired(), Email()]
+    )
+
+    picture = FileField(label='Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username already taken! Please choose an another username.")
+
+    def validate_email(self, email):
+        if email != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError("An User with that email exists. Please provide a different email!")
--- flaskBlog/routes.py
+++ flaskBlog/routes.py
@@ -1,8 +1,12 @@
 from flask import render_template, redirect, flash, url_for, request
 from flaskBlog import app, bcrypt, db
-from flaskBlog.forms import LoginForm, RegistrationForm
+from flaskBlog.forms import LoginForm, RegistrationForm, UpdateAccountForm
 from flaskBlog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+from PIL import Image
+import secrets
+import os
+
 
 posts = [
     {
@@ -10,6 +14,78 @@ posts = [
         'content': 'This is my first post',
         'author': 'Kausik',
         'date': '28/11/24'
+    },
+    {
+        'title': 'Second Post',
+        'content': 'This is my second post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Third Post',
+        'content': 'This is my third post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Fourth Post',
+        'content': 'This is my fourth post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Fifth Post',
+        'content': 'This is my fifth post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Sixth Post',
+        'content': 'This is my sixth post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Seventh Post',
+        'content': 'This is my seventh post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Eighth Post',
+        'content': 'This is my eighth post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Ninth Post',
+        'content': 'This is my ninth post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Tenth Post',
+        'content': 'This is my tenth post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Eleventh Post',
+        'content': 'This is my eleventh post',
+        'author': 'Kausik',
+        'date': '28/11/24'
+    },
+    {
+        'title': 'Twelfth Post',
+        'content': 'This is my twelfth post',
+        'author': 'Kausik',
+        'date': '2/12/24'
+    },
+    {
+        'title': 'Thirteenth Post',
+        'content': 'This is my thirteenth post',
+        'author': 'Kausik',
+        'date': '2/12/24'
     }
 ]
 
@@ -67,13 +143,44 @@ def login():
     return render_template('login.html', title="Login", form=form)
 
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('home'))
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, file_format = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex+file_format
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_filename)
+    output_size = (125, 125)
+    img = Image.open(form_picture)
+    img.thumbnail(output_size)
+    img.save(picture_path)
+    return picture_filename
+
+
 @app.route('/account', methods=["GET","POST"])
 @login_required
 def account():
-    return render_template('account.html', title='account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            current_picture = current_user.image_file
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+            os.remove(current_picture)
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash(f"Your account has been updated", 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename="profile_pics/"+current_user.image_file)
+    return render_template('account.html', title='account', image_file=image_file, form=form)
+
+
